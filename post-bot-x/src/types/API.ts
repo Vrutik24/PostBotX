26827d35b9af7948741c7d591b3dc3c4819c9258
Vrutik Rana <vrutik.rana@allata.com>
2024-08-29 03:30:40 +0530

    Minor type change

--- post-bot-x/src/types/API.ts
+++ post-bot-x/src/types/API.ts
@@ -1,19 +1,19 @@
-import {Header} from "./Header";
+import { Header } from "./Header";
 import { QueryParameter } from "./QueryParameter";
 
 export interface API {
-    id:               string;
-    name:             string;
-    createdById:      string; 
-    updatedById :     string;
-    createdOn:        Date;
-    updatedOn:        Date;
-    collectionId:     string;
-    apiType:          string;
-    isAutomated:      boolean;
-    url:              string;
-    configuredPayload?:string;
-    payload?:          string[];
-    headers?:          Header[];
-    queryParameters?:  QueryParameter[];
+  id: string;
+  name: string;
+  createdById: string;
+  updatedById: string;
+  createdOn: Date;
+  updatedOn: Date;
+  collectionId: string;
+  apiType: string;
+  isAutomated: boolean;
+  url: string;
+  configuredPayload?: string;
+  payload?: string[];
+  headers?: Header[];
+  queryParameters?: QueryParameter[];
 }
