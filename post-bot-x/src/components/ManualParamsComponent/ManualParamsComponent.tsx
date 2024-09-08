b46924402b6e911c3aa4a337d502be2a6d4162a7
Vrutik24 <68278532+Vrutik24@users.noreply.github.com>
2024-09-08 22:57:09 +0530

    Manual Parameters Checkbox update changes are done (#40)
    Co-authored-by: Vrutik Rana <vrutik.rana@allata.com>

--- post-bot-x/src/components/ManualParamsComponent/ManualParamsComponent.tsx
+++ post-bot-x/src/components/ManualParamsComponent/ManualParamsComponent.tsx
@@ -32,19 +32,93 @@ const ManualParamsComponent = () => {
       resetRowFlags();
     } else {
       const newQueryParameters = formik.values.manualQueryParameters.filter(
-        (
-          queryParameter: {
-            key: string;
-            value: string[];
-          },
-          index
-        ) => index !== i
+        (queryParameter, index) => index !== i
       );
       formik.setFieldValue("manualQueryParameters", newQueryParameters);
       resetRowFlags();
     }
   };
 
+  // Helper function to check if all values in the array are empty or undefined
+  const areAllValuesEmpty = (valueArray: string[]) => {
+    return valueArray.every((value) => !value || value === "");
+  };
+
+  const isRowEmpty = (
+    manualQueryParameters: {
+      key: string;
+      value: string[];
+    }[],
+    index: number,
+    field: "key" | "value",
+    value: string
+  ) => {
+    return (
+      isEmptyField(value) &&
+      (field === "key"
+        ? areAllValuesEmpty(manualQueryParameters[index].value)
+        : isEmptyField(manualQueryParameters[index].key))
+    );
+  };
+
+  const isEmptyField = (value: string) => !value || value === "";
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    index: number,
+    field: "key" | `value.${number}`
+  ) => {
+    const { manualQueryParameters } = formik.values;
+    const isLastRow = index === manualQueryParameters.length - 1;
+    const newValue = e.target.value;
+
+    // For checking the specific value field
+    const fieldKey = field.startsWith("value") ? "value" : "key";
+
+    // Check if the key or any value is empty, uncheck the checkbox if true
+    if (fieldKey === "key" && isEmptyField(newValue)) {
+      manualQueryParameters[index].isChecked = false;
+    } else if (
+      fieldKey === "value" &&
+      areAllValuesEmpty(manualQueryParameters[index].value)
+    ) {
+      manualQueryParameters[index].isChecked = false;
+    } else {
+      manualQueryParameters[index].isChecked = true;
+    }
+
+    if (
+      isRowEmpty(manualQueryParameters, index, fieldKey, newValue) &&
+      !isLastRow
+    ) {
+      deleteManualQueryParameter(index);
+    } else {
+      if (
+        isLastRow &&
+        !rowAddedFlags[index] &&
+        (manualQueryParameters[index].key || newValue)
+      ) {
+        setRowAddedFlags((prevFlags) => {
+          const updatedFlags = [...prevFlags];
+          updatedFlags[index] = true;
+          return updatedFlags;
+        });
+        formik.setFieldValue("manualQueryParameters", [
+          ...manualQueryParameters,
+          { key: "", value: [""] },
+        ]);
+      }
+
+      formik.setFieldValue(`manualQueryParameters.${index}.${field}`, newValue);
+    }
+  };
+
+  const handleCheckboxChange = (index: number) => {
+    const manualQueryParameters = [...formik.values.manualQueryParameters];
+    manualQueryParameters[index].isChecked =
+      !manualQueryParameters[index].isChecked; // Toggle the checked state
+    formik.setFieldValue("manualQueryParameters", manualQueryParameters); // Update formik state with new headers
+  };
   const addValueTextField = (index: number, valueArray: string[]) => {
     formik.setFieldValue(`manualQueryParameters.${index}.value`, [
       ...valueArray,
@@ -59,170 +133,148 @@ const ManualParamsComponent = () => {
     formik.setFieldValue(`manualQueryParameters.${index}.value`, newValueArray);
   };
 
-  const handleCheckboxChange = (index: number) => {
-    const manualQueryParameters = [...formik.values.manualQueryParameters];
-    manualQueryParameters[index].isChecked =
-      !manualQueryParameters[index].isChecked; // Toggle the checked state
-    formik.setFieldValue("manualQueryParameters", manualQueryParameters); // Update formik state with new headers
-  };
-
   return (
     <Table>
       <TableBody>
-        {formik.values.manualQueryParameters.map(
-          (
-            param: {
-              key: string;
-              value: string[];
-              isChecked: boolean;
-            },
-            index: number
-          ) => (
-            <TableRow
-              key={index}
-              sx={{
-                verticalAlign: "middle",
-                "&:hover .delete-icon": {
-                  visibility: "visible",
-                },
-              }}
+        {formik.values.manualQueryParameters.map((param, index) => (
+          <TableRow
+            key={index}
+            sx={{
+              verticalAlign: "middle",
+              "&:hover .delete-icon": {
+                visibility: "visible",
+              },
+            }}
+          >
+            <TableCell
+              sx={{ borderBottom: "none", width: "24px", verticalAlign: "top" }}
             >
-              <TableCell sx={{ borderBottom: "none", width: "24px" }}>
-                <Checkbox
-                  checked={
-                    param.key !== "" &&
-                    param.value.some((x) => x !== "") &&
-                    param.isChecked
-                  }
-                  onChange={() => handleCheckboxChange(index)}
-                  sx={{
+              <Checkbox
+                checked={
+                  param.key !== "" &&
+                  param.value.some((x) => x !== "") &&
+                  param.isChecked
+                }
+                onChange={() => handleCheckboxChange(index)}
+                sx={{
+                  color: "#FFFFFF",
+                  padding: 0,
+                  paddingTop: "8px",
+                  "&.Mui-checked": {
                     color: "#FFFFFF",
-                    padding: 0,
-                    "&.Mui-checked": {
-                      color: "#FFFFFF",
-                    },
-                  }}
-                />
-              </TableCell>
-              <TableCell sx={{ borderBottom: "none" }}>
-                <OutlinedInput
-                  value={`${formik.values.manualQueryParameters[index].key}`}
-                  id={`manualQueryParameters.${index}.key`}
-                  name={`manualQueryParameters.${index}.key`}
-                  sx={{
-                    height: "40px",
-                    border: "2px solid #2b2b2b",
+                  },
+                }}
+              />
+            </TableCell>
+            <TableCell sx={{ borderBottom: "none", verticalAlign: "top" }}>
+              <OutlinedInput
+                value={`${formik.values.manualQueryParameters[index].key}`}
+                id={`manualQueryParameters.${index}.key`}
+                name={`manualQueryParameters.${index}.key`}
+                sx={{
+                  height: "40px",
+                  border: "2px solid #2b2b2b",
 
-                    "& .MuiInputBase-input": {
-                      color: "#FFA24E",
-                    },
-                    "& .MuiInputBase-input::placeholder": {
-                      color: "gray",
-                      opacity: 1,
+                  "& .MuiInputBase-input": {
+                    color: "#FFA24E",
+                  },
+                  "& .MuiInputBase-input::placeholder": {
+                    color: "gray",
+                    opacity: 1,
+                  },
+                }}
+                onChange={(e) => handleInputChange(e, index, "key")}
+                placeholder="Key"
+                fullWidth
+              />
+            </TableCell>
+            {param.value.map((val, valIndex) => (
+              <Box display={"flex"} flexDirection={"column"} gap={"20px"}>
+                <TableCell
+                  key={valIndex}
+                  sx={{
+                    borderBottom: "none",
+                    "&:hover .delete-value-icon": {
+                      visibility: "visible",
                     },
                   }}
-                  onChange={(e) => {
-                    if (
-                      index ===
-                        formik.values.manualQueryParameters.length - 1 &&
-                      !rowAddedFlags[index] &&
-                      e.target.value
-                    ) {
-                      setRowAddedFlags((prevFlags) => {
-                        const updatedFlags = [...prevFlags];
-                        updatedFlags[index] = true;
-                        return updatedFlags;
-                      });
-                      formik.setFieldValue("manualQueryParameters", [
-                        ...formik.values.manualQueryParameters,
-                        { key: "", value: [""] },
-                      ]);
-                    }
-                    formik.setFieldValue(
-                      `manualQueryParameters.${index}.key`,
-                      e.target.value
-                    );
-                  }}
-                  placeholder="Key"
-                  fullWidth
-                />
-              </TableCell>
-              {param.value.map((val: string, valIndex: number) => (
-                <Box display={"flex"} flexDirection={"column"} gap={"20px"}>
-                  <TableCell key={valIndex} sx={{ borderBottom: "none" }}>
-                    <OutlinedInput
-                      value={val}
-                      id={`manualQueryParameters.${index}.value.${valIndex}`}
-                      name={`manualQueryParameters.${index}.value.${valIndex}`}
-                      sx={{
-                        height: "40px",
-                        border: "2px solid #2b2b2b",
-                        "& .MuiInputBase-input": {
-                          color: "#FFF",
-                        },
-                        "& .MuiInputBase-input::placeholder": {
-                          color: "gray",
-                          opacity: 1,
-                        },
-                      }}
-                      onChange={(e) => {
-                        formik.setFieldValue(
-                          `manualQueryParameters.${index}.value.${valIndex}`,
-                          e.target.value
-                        );
-                      }}
-                      endAdornment={
-                        valIndex === 0 ? (
-                          <InputAdornment position="end">
-                            <IconButton
-                              sx={{ color: "gray" }}
-                              size="small"
-                              onClick={() =>
-                                addValueTextField(index, param.value)
-                              }
-                            >
-                              <AddCircleOutline />
-                            </IconButton>
-                          </InputAdornment>
-                        ) : (
-                          <InputAdornment position="end">
-                            <IconButton
-                              sx={{ color: "gray" }}
-                              size="small"
-                              onClick={() =>
-                                deleteValueTextField(index, valIndex)
-                              }
-                            >
-                              <Delete />
-                            </IconButton>
-                          </InputAdornment>
-                        )
-                      }
-                      placeholder="Value"
-                      fullWidth
-                    />
-                  </TableCell>
-                </Box>
-              ))}
-              <TableCell sx={{ borderBottom: "none", padding: 0 }}>
-                {formik.values.manualQueryParameters.length > 1 && (
-                  <Delete
-                    className="delete-icon"
+                >
+                  <OutlinedInput
+                    value={val}
+                    id={`manualQueryParameters.${index}.value.${valIndex}`}
+                    name={`manualQueryParameters.${index}.value.${valIndex}`}
                     sx={{
-                      cursor: "pointer",
-                      color: "gray",
-                      fontSize: 20,
-                      visibility: "hidden",
+                      height: "40px",
+                      border: "2px solid #2b2b2b",
+                      "& .MuiInputBase-input": {
+                        color: "#FFF",
+                      },
+                      "& .MuiInputBase-input::placeholder": {
+                        color: "gray",
+                        opacity: 1,
+                      },
                     }}
-                    onClick={() => {
-                      deleteManualQueryParameter(index);
-                    }}
+                    onChange={(e) =>
+                      handleInputChange(e, index, `value.${valIndex}`)
+                    }
+                    endAdornment={
+                      valIndex === 0 ? (
+                        <InputAdornment position="end">
+                          <IconButton
+                            sx={{ color: "gray" }}
+                            size="small"
+                            onClick={() =>
+                              addValueTextField(index, param.value)
+                            }
+                          >
+                            <AddCircleOutline />
+                          </IconButton>
+                        </InputAdornment>
+                      ) : (
+                        <InputAdornment position="end">
+                          <IconButton
+                            className="delete-value-icon"
+                            sx={{
+                              color: "gray",
+                              cursor: "pointer",
+                              fontSize: 20,
+                              visibility: "hidden",
+                            }}
+                            size="small"
+                            onClick={() =>
+                              deleteValueTextField(index, valIndex)
+                            }
+                          >
+                            <Delete />
+                          </IconButton>
+                        </InputAdornment>
+                      )
+                    }
+                    placeholder="Value"
+                    fullWidth
                   />
-                )}
-              </TableCell>
-            </TableRow>
-          )
-        )}
+                </TableCell>
+              </Box>
+            ))}
+            <TableCell
+              sx={{ borderBottom: "none", padding: 0, verticalAlign: "top" }}
+            >
+              {formik.values.manualQueryParameters.length > 1 && (
+                <Delete
+                  className="delete-icon"
+                  sx={{
+                    cursor: "pointer",
+                    color: "gray",
+                    fontSize: 20,
+                    visibility: "hidden",
+                    paddingTop: "24px",
+                  }}
+                  onClick={() => deleteManualQueryParameter(index)}
+                />
+              )}
+            </TableCell>
+          </TableRow>
+        ))}
       </TableBody>
     </Table>
   );
