1d464a80ae6d00f8ed4b258722cef6153c270889
Vrutik Rana <vrutik.rana@allata.com>
2024-09-10 15:51:14 +0530

    New Firebase config added

--- post-bot-x/src/firebase.ts
+++ post-bot-x/src/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDO8NUZVVOcC3jy44VwsWKZGkk92yo1Mjk",
-  authDomain: "postbotx.firebaseapp.com",
-  projectId: "postbotx",
-  storageBucket: "postbotx.appspot.com",
-  messagingSenderId: "734070725033",
-  appId: "1:734070725033:web:aed4ea1891a34767880cb8"
+  apiKey: "AIzaSyCK6Pi5dxTG4lxWvRSulZksx5jyHLV2SlM",
+  authDomain: "post-bot-x-2e5ec.firebaseapp.com",
+  projectId: "post-bot-x-2e5ec",
+  storageBucket: "post-bot-x-2e5ec.appspot.com",
+  messagingSenderId: "137470613298",
+  appId: "1:137470613298:web:2353dff5375bf8a330692b"
 };
 
 // Initialize Firebase
